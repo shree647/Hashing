f4e0af7f071bd0d16d1888084796da65b772dfa9
Shreenidhi <shreenidhic2002@gmail.com>
2022-12-25 23:42:27 +0530

    output is showing in terminal

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
 from tkinter import *
-#from main import *
+import os
+import subprocess
+from main import *
+from tkinter import filedialog
+from tkinter.filedialog import askopenfilename
 from subprocess import call
 
+def show():
+    pro = filedialog.askopenfilename()
+    #l2.config(text=pro)
+    os.system('"%s"'% pro)
+
+
 def open_py_file():
-    call(["python", "execution.py"])
+    call(["python", "main.py"])
+
+def output1():
+    o = subprocess.check_output(["python", "C:\\Users\\shree\\OneDrive\\Pictures\\Documents\\python assignment\\main.py>"])
+    Label(root, text=o).pack()
 
 root = Tk()
 root.geometry('300x300')
@@ -11,13 +25,13 @@ root.geometry('300x300')
 l = Label(root, text = "HASHING")
 l.pack()
 
-b = Button(root, text="start" , command=open_py_file)
+l2 = Label(root, text="")
+l2.pack()
+
+b = Button(root, text="start" , command=show)
 b.pack()
 
-b1 = Button(root, text="output" )
+b1 = Button(root, text="output")
 b1.pack()
-
-b2 = Button(root, text="stop")
-b2.pack()
 
 root.mainloop()
